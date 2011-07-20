273488ace8bcb0b94bfb0ae6e62ae78ab7444b15
xinxinzhang <xinxinzhang22@gmail.com>
2011-07-20 17:58:04 +0800

    add weibo php sdk

--- member/source.php
+++ member/source.php
@@ -1,55 +1,30 @@
 <?php
 include "../global.php";
+session_start();
+include_once( '../weibo/config.php' );
+include_once( '../weibo/weibooauth.php' );
+
+$o = new WeiboOAuth( WB_AKEY , WB_SKEY  );
+
+$keys = $o->getRequestToken();
+//$aurl = $o->getAuthorizeURL( $keys['oauth_token'] ,false , $_SERVER['SCRIPT_URI'].'/callback.php');
+//$aurl = $o->getAuthorizeURL( $keys['oauth_token'] ,false , $_SERVER['REQUEST_URI'].'/callback.php');
+
+$aurl = $o->getAuthorizeURL( $keys['oauth_token'] ,false , 'http://story.com/storify/weibo/callback.php');
+
+$_SESSION['keys'] = $keys;
 ?>
 
 <div class='div_center' >
 <span>请您选择要添加的信息源</span>
 <ul>
-  <li id='sina_weibo'><span id="connectBtn"></span></li>
+  <li id='sina_weibo'><a href="<?=$aurl?>">新浪微博</a></li>
   <li id='tencent_weibo'><a>腾讯微博</a></li>
   <li id='renren'><a>人人网</a></li>
 </ul>
 <ul id='source_info'></ul>
 </div>
 
-<script type="text/javascript">
-$(function(){
-WB.core.load(['connect', 'client', 'widget.base', 'widget.atWhere'], function() 
-{
-  var cfg = {
-              //key: '314237338',
-			  key: '2417356638',
-			  xdpath: 'http://story.com/storify/html/xd.html'
-			};
-  WB.connect.init(cfg);
-  WB.client.init(cfg);
-});
-
-
-
-})
-
-window.onload = function(){
-WB.widget.base.connectButton(document.getElementById('connectBtn'),
-{							     
- login:function(o)
- {
-   //debugger;
-   var postdata = {weibo_user_id: o.id};
-   $.post('updateuser.php', postdata,
-	 function(data, textStatus)
-	 {
-	   var $info = ("<li>添加了新浪微博</li>");
-	   $('#source_info').append($info);								
-	 });
- },
- logout:function()
- {
-
- }
-});
-}
-</script>
 
 <?php
 include "../include/footer.htm";
---
+++ weibo/config.php
@@ -0,0 +1,5 @@
+<?php
+define( "WB_AKEY" , '2417356638' );
+define( "WB_SKEY" , '98b2edceea41d422af18bd7fcbb11c70' );
+
+?>
