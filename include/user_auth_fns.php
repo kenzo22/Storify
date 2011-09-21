8f669bb5f1711a3927aa35f2140a5143ce47466c
xinxinzhang <xinxinzhang22@gmail.com>
2011-09-21 15:35:05 +0800

    add paging function for user.php user's story list

--- include/user_auth_fns.php
+++ include/user_auth_fns.php
@@ -1,5 +1,5 @@
 <?php 
-require_once dirname(__FILE__).'/../'."connect_db.php";  
+require_once dirname(__FILE__).'/../'."connect_db.php"; 
 
 function islogin()
 {
--- member/user.php
+++ member/user.php
@@ -586,11 +586,115 @@ else if(isset($_GET['post_id']) && isset($_GET['action']))
 
 else if(isset($_GET['user_id']))
 {
+  $tbl_name="story_posts";
+  // How many adjacent pages should be shown on each side?
+  $adjacents = 3;
   $user_id = $_GET['user_id'];
   $story_content = "<div id='userstory_container' class='inner'><div class='userstory_list'><ul>";
-  $result=$DB->query("SELECT * FROM ".$db_prefix."posts where post_author='".$user_id."'");
   $userresult = $DB->fetch_one_array("SELECT username, photo FROM ".$db_prefix."user where id='".$user_id."'");
   $user_profile_img = $userresult['photo'];
+  
+    $query = "SELECT COUNT(*) as num FROM $tbl_name where post_author='".$user_id."'";
+	$total_pages = mysql_fetch_array(mysql_query($query));
+	$total_pages = $total_pages[num];
+	
+	$targetpage = "user.php?user_id=".$user_id; 
+	$limit = 12; 								//how many items to show per page
+	$page = $_GET['page'];
+	if($page) 
+		$start = ($page - 1) * $limit; 			//first item to display on this page
+	else
+		$start = 0;								//if no page var is given, set start to 0
+	
+	/* Get data. */
+	$sql = "SELECT * FROM $tbl_name where post_author='".$user_id."'LIMIT $start, $limit";
+	$result = mysql_query($sql);
+	
+	/* Setup page vars for display. */
+	if ($page == 0) $page = 1;					//if no page var is given, default to 1.
+	$prev = $page - 1;							
+	$next = $page + 1;							
+	$lastpage = ceil($total_pages/$limit);	
+	$lpm1 = $lastpage - 1;						//last page minus 1
+	
+	$pagination = "";
+	if($lastpage > 1)
+	{	
+		$pagination .= "<div class=\"pagination\">";
+		//previous button
+		if ($page > 1) 
+			$pagination.= "<a href=\"$targetpage&page=$prev\">« 前页</a>";
+		else
+			$pagination.= "<span class=\"disabled\">« 前页</span>";	
+		
+		//pages	
+		if ($lastpage < 7 + ($adjacents * 2))	//not enough pages to bother breaking it up
+		{	
+			for ($counter = 1; $counter <= $lastpage; $counter++)
+			{
+				if ($counter == $page)
+					$pagination.= "<span class=\"current\">$counter</span>";
+				else
+					$pagination.= "<a href=\"$targetpage&page=$counter\">$counter</a>";					
+			}
+		}
+		elseif($lastpage > 5 + ($adjacents * 2))	//enough pages to hide some
+		{
+			//close to beginning; only hide later pages
+			if($page < 1 + ($adjacents * 2))		
+			{
+				for ($counter = 1; $counter < 4 + ($adjacents * 2); $counter++)
+				{
+					if ($counter == $page)
+						$pagination.= "<span class=\"current\">$counter</span>";
+					else
+						$pagination.= "<a href=\"$targetpage&page=$counter\">$counter</a>";					
+				}
+				$pagination.= "...";
+				$pagination.= "<a href=\"$targetpage&page=$lpm1\">$lpm1</a>";
+				$pagination.= "<a href=\"$targetpage&page=$lastpage\">$lastpage</a>";		
+			}
+			//in middle; hide some front and some back
+			elseif($lastpage - ($adjacents * 2) > $page && $page > ($adjacents * 2))
+			{
+				$pagination.= "<a href=\"$targetpage&page=1\">1</a>";
+				$pagination.= "<a href=\"$targetpage&page=2\">2</a>";
+				$pagination.= "...";
+				for ($counter = $page - $adjacents; $counter <= $page + $adjacents; $counter++)
+				{
+					if ($counter == $page)
+						$pagination.= "<span class=\"current\">$counter</span>";
+					else
+						$pagination.= "<a href=\"$targetpage&page=$counter\">$counter</a>";					
+				}
+				$pagination.= "...";
+				$pagination.= "<a href=\"$targetpage&page=$lpm1\">$lpm1</a>";
+				$pagination.= "<a href=\"$targetpage&page=$lastpage\">$lastpage</a>";		
+			}
+			//close to end; only hide early pages
+			else
+			{
+				$pagination.= "<a href=\"$targetpage&page=1\">1</a>";
+				$pagination.= "<a href=\"$targetpage&page=2\">2</a>";
+				$pagination.= "...";
+				for ($counter = $lastpage - (2 + ($adjacents * 2)); $counter <= $lastpage; $counter++)
+				{
+					if ($counter == $page)
+						$pagination.= "<span class=\"current\">$counter</span>";
+					else
+						$pagination.= "<a href=\"$targetpage&page=$counter\">$counter</a>";					
+				}
+			}
+		}
+		
+		//next button
+		if ($page < $counter - 1) 
+			$pagination.= "<a href=\"$targetpage&page=$next\">后页 »</a>";
+		else
+			$pagination.= "<span class=\"disabled\">后页 »</span>";
+		$pagination.= "</div>\n";		
+	}
+  
   while ($story_item = mysql_fetch_array($result))
   {
     //printf ("title: %s  summary: %s", $story_item['post_title'], $story_item['post_summary']);
@@ -601,7 +705,6 @@ else if(isset($_GET['user_id']))
 	$post_date = $story_item['post_date'];
 	$temp_array = explode(" ", $story_item['post_date']);
 	$post_date = $temp_array[0];
-	//if(!islogin() || $story_author != $_SESSION['uid'])
     $story_content .= "<li><div class='story_wrap'><a class='cover' style='background: url(".$post_pic_url.") no-repeat; background-size: 100%;' href='/member/user.php?post_id=".$story_item['ID']."'><div class='title_wrap'><h1 class='title'>".$post_title."</h1></div></a><div class='editable'>
   <div class='status'>
     <div class='".$post_status."'>
@@ -617,13 +720,13 @@ else if(isset($_GET['user_id']))
 	  <a class='icon edit' title='编辑' href='/member/index.php?post_id=".$post_id."'><img src='../img/edit.png' style='width:16px; height:16px;'/></a>
     </div>";
   }
-   $story_content .="<div class='clear'></div>
-</div></div>
+   $story_content .="<div class='clear'></div></div></div>
 	<div class='story_meta'><span><img border='0' style='position:relative; top:3px; width: 20px; height:20px;' src='".$user_profile_img."'/><a style='margin-left:5px; vertical-align:top;'>".$userresult['username']."</a><a style='margin-left:65px; vertical-align:top;'>".$post_date."</a></span></div></li>";
   }
 
-  $story_content .="</ul></div></div>";
+  $story_content .="</ul></div>".$pagination."</div>";
   echo $story_content;
+  //echo $pagination;
 }
 ?>
 
