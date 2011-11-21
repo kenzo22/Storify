41099dc46f450f2743e1b830c774579775383926
jinhuan <crazyscar@gmail.com>
2011-11-21 23:24:05 +0800

    move login, register, settings, and source to accounts dir. They are user-related.

--- story_weibo.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS story_weibo (
-  weibo_ID bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  weibo_permanent_ID bigint(20) unsigned NOT NULL DEFAULT '0',
-  weibo_post_ID bigint(20) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`weibo_ID`),
-  KEY `weibo_post_ID` (`weibo_post_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
