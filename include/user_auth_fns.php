75e35b410352284436cb742bd2bc800596036ec8
jinhuan <crazyscar@gmail.com>
2011-12-04 23:29:23 +0800

    public token

--- include/user_auth_fns.php
+++ include/user_auth_fns.php
@@ -1,6 +1,13 @@
 <?php 
 require_once $_SERVER['DOCUMENT_ROOT']."/connect_db.php"; 
 
+$weibo_access_token =           array('3dded3c1a69e0e24609b04c3bc07d3ee', 'a5a036de79ad7bb7e71446366d9c69ab', '9a0db78eaffe82ee099f17c8937f29cf');
+$weibo_access_token_secret =    array('4815f86a2f8dcbbca4a307535b1a82d8', 'ddd74ff5df9a06325822cefdec81e10e', '0175d039c755cc3b128c134f30b9af3c');
+
+$tweibo_access_token =          array('1fce15f8b9d3449ea9a031adf9138f95', '4fc29d6f9721471fabfb38ce56298f48');
+$tweibo_access_token_secret =   array('2a4a03d0dac0951f06d3e7b5b30a1ea0', '355354af7961e5bbc154238dca72a75a');
+
+
 function islogin()
 {
  global $_SESSION;
@@ -80,17 +87,23 @@ function getUserPic($uid)
 
 function getPublicToken()
 {
-  global $_SESSION;
-  global $DB;
-  global $db_prefix;
-  $token = $DB->fetch_one_array("select * from ".$db_prefix."publictoken where id='1'");
+    global $weibo_access_token, $tweibo_access_token, $weibo_access_token_secret, $tweibo_access_token_secret;
+    global $_SESSION;
   if($_SESSION['last_wkey']['oauth_token'] == '')
   {
-	$_SESSION['last_wkey']['oauth_token'] = $token['weibo_access_token'];
-	$_SESSION['last_wkey']['oauth_token_secret'] = $token['weibo_access_token_secret'];
+        $max = sizeof($weibo_access_token);
+        $indx = rand(0,$max-1);
+	$_SESSION['last_wkey']['oauth_token'] = $weibo_access_token[$indx];
+	$_SESSION['last_wkey']['oauth_token_secret'] =  $weibo_access_token_secret[$indx];
   }
-  $_SESSION['last_tkey']['oauth_token'] = $token['tweibo_access_token'];
-  $_SESSION['last_tkey']['oauth_token_secret'] = $token['tweibo_access_token_secret'];
+
+    if($_SESSION['last_tkey']['oauth_token'] == '')
+    {
+        $max = sizeof($tweibo_access_token);
+        $indx = rand(0, $max-1);
+        $_SESSION['last_tkey']['oauth_token'] =  $tweibo_access_token[$indx];
+        $_SESSION['last_tkey']['oauth_token_secret'] = $tweibo_access_token_secret[$indx];
+    }
 }
 
 ?>
